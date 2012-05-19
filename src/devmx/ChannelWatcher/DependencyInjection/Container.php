75658b8438797b7aae856a80e84279dd7e5179ae
drak3 <drak3@live.de>
2012-05-19 17:02:53 +0200

    Added dbal services

--- src/devmx/ChannelWatcher/DependencyInjection/Container.php
+++ src/devmx/ChannelWatcher/DependencyInjection/Container.php
@@ -22,6 +22,15 @@ class Container extends \Pimple
         $this['dbal.connection'] = $this->share(function($c){
             return \Doctrine\DBAL\DriverManager::getConnection($c['dbal.connection.params']);
         });
+        $this['dbal.db_manager'] = $this->share(function($c) {
+            return new \devmx\ChannelWatcher\Storage\DbalStorage\DataBaseManager;
+        });
+        
+        $this['dbal.table_name'] = 'devmx_channel_deleter';
+        
+        $this['dbal.storage'] = $this->share(function($c){
+            return new \devmx\ChannelWatcher\Storage\DbalStorage\DbalStorage($c['dbal.connection'], $c['dbal.table_name']);
+        });
         
         $this['deleter'] = $this->share(function($c) {
             return new ChannelDeleter();
@@ -37,14 +46,26 @@ class Container extends \Pimple
             return new CrawlCommand($c['crawler']);
         };
         $this['command.create_db'] = function($c) {
-            return new CreateDataBaseCommand($c['dbal']['connection'], $c['dbal']['db_manager'], $c['dbal']['table_name']);
+            return new CreateDataBaseCommand($c['dbal.connection'], $c['dbal.db_manager'], $c['dbal.table_name']);
         };
         $this['command.print_deletable'] = function($c) {
           return new ToDeletePrintCommand($c['storage']);
         };
-        $this['storage'] = function($c) {
-            return $c['dbal']['storage'];
+        
+        $this['application'] = function($c) {
+          $app = new \Symfony\Component\Console\Application($c['application.name'], $c['application.version']);
+          $app->addCommands(array($c['command.crawl'], $c['command.create_db'], $c['command.print_deletable']));
+          return $app;
         };
+        
+        $this['application.name'] = 'devmx ChannelWatcher';
+        
+        $this['application.version'] = '0.1';
+        
+        $this['storage'] = function($c) {
+            return $c['dbal.storage'];
+        };
+        
         $this['storage.in_memory'] = $this->share(function($c){
             return new \devmx\ChannelWatcher\Storage\InMemoryStorage;
         });
