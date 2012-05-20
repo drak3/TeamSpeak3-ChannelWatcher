157be7e272fe314ddf0d14e640e36cf1f3e1d188
drak3 <drak3@live.de>
2012-05-20 14:18:33 +0200

    Added DateConverter skeleton + tests (#3)

---
+++ src/devmx/ChannelWatcher/DateConverter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace devmx\ChannelWatcher;
+
+/**
+ *
+ * @author drak3
+ */
+class DateConverter
+{
+    /**
+     * Converts a timespec to a DateInterval
+     * @param array $timeSpec possible keys: years, months, weeks, days, hours, minutes, seconds
+     * e.g:
+     * array(
+     *  'weeks' => 1
+     *  'days' => 12
+     * );          
+     */
+    public static function convertArrayToInterval(array $timeSpec) {
+        
+    }
+}
+
+?>
