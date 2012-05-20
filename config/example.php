0cb3d275f4839507f16e93190a5cf842554d5901
drak3 <drak3@live.de>
2012-05-20 13:08:00 +0200

    Cleaned up example config

--- config/example.php
+++ config/example.php
@@ -3,8 +3,8 @@
 $c['ts3']['host'] = 'localhost';
 $c['ts3']['query.port'] = 10011;
 $c['ts3']['vserver.port'] = 9987;
-$c['ts3']['login.name'] = 'asdf';
-$c['ts3']['login.pass'] = 'oVRXzIrt';
+$c['ts3']['login.name'] = '';
+$c['ts3']['login.pass'] = '';
 $c['delete_time'] = new \DateInterval('PT100S');
 
 $c['dbal.connection.params'] = array(
