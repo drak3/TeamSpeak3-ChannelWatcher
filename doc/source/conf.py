72f3d94eb219d3f604e74ef873b7d4501b9099ad
Maximilian Narr <maxe94@live.de>
2012-05-31 18:32:07 +0200

    updated doc config and added some troubleshooting

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -83,7 +83,6 @@ exclude_patterns = []
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
 
@@ -147,10 +146,10 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
