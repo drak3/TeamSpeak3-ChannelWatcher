2e62c32bef5ce3c96b9f475a281bb2af293ad4cb
drak3 <drak3@live.de>
2012-06-02 01:10:47 +0200

    Removed unnessecary file

--- autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'vendor/.composer/autoload.php';
-$loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespace( 'devmx\ChannelWatcher', array(__DIR__.'/src/') );
-$loader->register();
-?>
