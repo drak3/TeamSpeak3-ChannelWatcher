979ed822098f7f23a70b6be0c55400f2440fa9c6
drak3 <drak3@live.de>
2012-07-24 19:49:51 +0200

    Improved packaging

--- make-package.sh
+++ make-package.sh
@@ -1,39 +1,44 @@
+
 #!/bin/bash
 
-if [ -d build/ ]
+if ! [ -d build/ ]
 then
 mkdir build
 fi
 
 cd build/
 
-read -p "Version:" VERSION
+read -p "Version: " VERSION
 
-if [-d $VERSION ]
+if ! [ -d $VERSION ]
 then
 mkdir $VERSION
 fi
+
+cd $VERSION
 
 cp -rf ../../* .
 
 rm -rf doc
 rm -rf tests
 
-wget getcomposer.org/composer.phar
+curl -s https://getcomposer.org/installer | php
 php composer.phar install
 
 find . -type d -name .git -print0 | xargs -0 -r rm -rf
 
-rm composer*
-rm phpunit*
-rm -r .travis*
-rm -r .git*
+rm composer.*
+rm phpunit.*
+rm .travis.yml
+rm .gitignore
+rm .gitmodules
 rm -rf nbproject
-
+rm -rf build
+rm -rf storage
+rm config/local.php
 #delete this script too
 rm make-package.sh
 
 
-cd ../
 
-zip -r "devMX TeamSpeak3 Webviewer v$version.zip" .
+zip -r "../devMX TeamSpeak3 Webviewer v$VERSION.zip" .
