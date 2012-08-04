a36e0288037a52faa55fafd26dd05734a62d3060
drak3 <drak3@live.de>
2012-08-05 00:39:25 +0200

    Moved db init to own script

---
+++ tests/travis-config/init-db.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+
+if [ '$DB' = 'pgsql' ] 
+then 
+    psql -c 'DROP DATABASE IF EXISTS devmx_channelwatcher_tests;' -U postgres
+fi
+
+if [ '$DB' = 'pgsql' ]
+then 
+    psql -c 'DROP DATABASE IF EXISTS devmx_channelwatcher_tests_tmp;' -U postgres
+fi
+
+if [ '$DB' = 'pgsql' ]
+then 
+    psql -c 'create database devmx_channelwatcher_tests;' -U postgres
+fi
+
+if [ '$DB' = 'pgsql' ]
+then 
+psql -c 'create database devmx_channelwatcher_tests_tmp;' -U postgres
+fi
+
+if [ '$DB' = 'mysqli' ]
+then 
+mysql -e 'create database IF NOT EXISTS devmx_channelwatcher_tests_tmp;create database IF NOT EXISTS devmx_channelwatcher_tests;'
+fi
